15cac56f236aef7e2f2d2583d9ae7d1b7807fe47
mpadge <mark.padgham@email.com>
2024-02-27 17:13:51 +0100

    interpolate all missing census vals to weighted nb vals

--- R/hardship-index.R
+++ R/hardship-index.R
@@ -25,12 +25,39 @@ hs_hardship_index <- function (state = "AZ", year = 2022, survey = "acs5") {
 
     dat <- m_hs_get_census_data (state = state, year = year, survey = survey)
 
-    dat$income <- max (dat$income, na.rm = TRUE) - dat$income
-    vars <- c ("occupancy", "poverty", "unemployment", "no_hs", "deps", "income")
+    # log-transform all data:
+    nms <- c ("GEOID", "NAME", "geometry")
+    vars <- names (dat) [which (!names (dat) %in% nms)]
+
     for (v in vars) {
+        dat [[v]] <- log10 (dat [[v]])
+        dat [[v]] [which (!is.finite (dat [[v]]))] <- NA
         vmin <- min (dat [[v]], na.rm = TRUE)
         vmax <- max (dat [[v]], na.rm = TRUE)
         dat [[v]] <- (dat [[v]] - vmin) / (vmax - vmin)
+    }
+
+    # Then impute any missing values:
+    xy_cent <- sf::st_as_sf (dat) |>
+        sf::st_transform (crs = 4326) |>
+        sf::st_centroid () |>
+        sf::st_coordinates ()
+
+    index <- which (!is.na (xy_cent [, 1]) & !is.na (xy_cent [, 2]))
+    xy_cent <- xy_cent [index, ]
+    dat <- dat [index, ]
+
+    # replace any NA values with weighted neighbour values:
+    for (v in vars) {
+        message (v)
+        index_NA <- which (is.na (dat [[v]]))
+        index <- which (!is.na (dat [[v]]))
+        dmin <- geodist::geodist (xy_cent [index_NA, ], xy_cent [index, ], measure = "haversine")
+        d_wts <- exp (-dmin / 1000)
+        d_wt_sums <- array (rowSums (d_wts), dim = t (dim (dmin)))
+        d_wts <- d_wts / d_wt_sums
+        dat_wt <- array (dat [[v]] [index], dim = t (dim (dmin)))
+        dat [[v]] [index_NA] <- rowSums (d_wts * dat_wt, na.rm = TRUE)
     }
 
     dat_mat <- as.matrix (dat [, vars])
